5027fd5305f7ea69fdaea077e6d5b8563b94af23
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-05-28 14:37:16 +0530

    add

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dotnet-framework-learnings
+
 This repo is to maintain the source code of my learning of dotnet framework and C#
---
+++ learnings/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace learnings
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Hello, World!");
+            Console.Write("Hello");
+            Console.ReadLine();
+        }
+    }
+}
